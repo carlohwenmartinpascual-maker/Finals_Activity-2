4aad6e525a6929757637c4c07be9a0d629ce3b4a
Carl ohwen martin F. Pascual <carlohwenmartinpascual@gmail.com>
2026-05-21 18:23:26 +0800

    Create MINI PROJECT.php

---
+++ MINI PROJECT.php
@@ -0,0 +1,9 @@
+<?php
+$n1 = 7;
+$n2  = 4;
+
+echo "Sum:" . ($n1 + $n2) . "<br>";
+echo "Difference:" . ($n1 - $n2) . "<br>";
+echo "Product:" . ($n1 * $n2) . "<br>";
+echo "Quotient:" . ($n1 / $n2);
+?>

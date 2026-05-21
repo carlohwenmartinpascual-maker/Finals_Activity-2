3679b07b9b96fc00b2ee2f4fcef352992fea9494
Carl ohwen martin F. Pascual <carlohwenmartinpascual@gmail.com>
2026-05-21 18:20:02 +0800

    Create Activity 5.php

---
+++ Activity 5.php
@@ -0,0 +1,10 @@
+<?php
+$a = 12;
+$b = 4;
+
+echo "a = $a" . "<br>" . "b = $b" . "<br><br>";
+echo "Addition:" . ($a + $b) . "<br>";
+echo "Subtraction:" . ($a - $b) . "<br>";
+echo "Multiplication:" . ($a * $b) . "<br>";
+echo "Division:" . ($a / $b);
+?>
